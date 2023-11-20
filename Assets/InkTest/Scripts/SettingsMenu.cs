085ecffa68c5f3b92da5bb03c4771782bf95d8f1
Ekaterina <88728363+shmyakkk@users.noreply.github.com>
2023-11-20 17:29:12 +0300

    Revert "--"
    This reverts commit 3fc851b53edee17062e2c2518e49bfa14f976a99, reversing
    changes made to 1d566ab1145fb6a982bdce109d51c52d96386341.

---
+++ Assets/InkTest/Scripts/SettingsMenu.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VNCreator;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public Toggle musicActiveToggle;
+    public GameObject musicManager;
+
+    void Start()
+    {
+        if (musicActiveToggle != null)
+        {
+            musicActiveToggle.isOn = GameOptions.musicActive;
+            musicActiveToggle.onValueChanged.AddListener(GameOptions.SetMusicActiveVolume);
+            musicActiveToggle.onValueChanged.AddListener(musicManager.GetComponent<MusicManager>().UpdateMusicActive);
+        }
+    }
+}
